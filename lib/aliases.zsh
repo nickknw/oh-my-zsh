1304f222e8c3c9d1463da34f0c2818d9fbcf42d3
Nick Knowlson <nick@nickknowlson.com>
2013-07-05 13:31:33 -0700

    stop oh my zsh from messing with ls

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -16,10 +16,6 @@ alias please='sudo'
 alias history='fc -l 1'
 
 # List direcory contents
-alias lsa='ls -lah'
-alias l='ls -la'
-alias ll='ls -l'
-alias la='ls -lA'
 alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
